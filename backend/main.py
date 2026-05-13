d522dc0391dac3d58102044de50fb369998d9b33
Deepak Mudili <deepakmudili@Deepaks-MacBook-Air.local>
2026-05-13 15:14:42 +0530

    Fix FastAPI File import

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, UploadFile, Form
+from fastapi import FastAPI, UploadFile, Form, File
 from fastapi.middleware.cors import CORSMiddleware
 
 from database import supabase
